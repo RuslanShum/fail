5c803dc13296dccbd4733d950de77820d04a9ed9
RuslanShum <schumailov.ruslan@yandex.ru>
2024-09-12 20:49:10 +0400

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+name = 'Ruslan'
+print('Name:',name)
+age = "Age:"
+a = 41
+print(age,a)
+age = a + 2
+print('New age:', age)
+is_student = "in Student:"
+is_student1 = bool (a < age)
+print(is_student,is_student1)
